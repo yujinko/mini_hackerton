64215e41c243cbd1b241a9b7167bf5ec054b7423
uj2xxn <youjin1724@naver.com>
2024-07-16 12:32:26 +0900

    영화 평점 남기기 추가

--- src/Detail.js
+++ src/Detail.js
@@ -3,14 +3,16 @@ import axios from "axios";
 import "./Detail.css";
 import { useParams } from "react-router-dom";
 import Nav from "./Nav";
+import StarRating from "./StarRating";
 
 function Detail() {
   const [movieData, setMovieData] = useState(null);
   const [selectedMovie, setSelectedMovie] = useState(true);
   const [comment, setComment] = useState("");
   const [comments, setComments] = useState([]);
-  const {movieId} = useParams()
-  console.log(movieId)
+  const [rating, setRating] = useState(0);
+  const { movieId } = useParams();
+  console.log(movieId);
 
   useEffect(() => {
     const fetchMovieData = async () => {
@@ -18,17 +20,15 @@ function Detail() {
         const response = await axios.get(
           `https://minihackton.store/movie/${movieId}/`
         );
-        setMovieData(response.data)
+        setMovieData(response.data);
         setComments(response.data.comments);
         console.log(response.data);
-        
       } catch (error) {
         console.error("Error fetching movie data:", error);
-        
       }
     };
     fetchMovieData();
-  }, []);
+  }, [movieId]);
 
   const handleMovieClick = (movie) => {
     setSelectedMovie(movie);
@@ -40,101 +40,129 @@ function Detail() {
 
   const handleCommentSubmit = async () => {
     try {
-      const token = localStorage.getItem('access')
+      const token = localStorage.getItem("access");
       await axios.post(
         `https://minihackton.store/movie/${movieId}/comment/`,
         {
           comment: comment,
-        },{
-          headers:{
-            Authorization: `Bearer ${token}`
-          }
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
         }
       );
       setComment("");
       const response = await axios.get(
         `https://minihackton.store/movie/${movieId}/`
       );
-      console.log(response)
+      console.log(response);
       setComments(response.data.comments);
     } catch (error) {
       console.error("Error submitting comment:", error);
     }
   };
 
+  const handleRatingSubmit = async (rating) => {
+    try {
+      const token = localStorage.getItem("access");
+      await axios.post(
+        `https://minihackton.store/movie/${movieId}/rating/`,
+        {
+          rating: rating,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      setRating(rating);
+      const response = await axios.get(
+        `https://minihackton.store/movie/${movieId}/`
+      );
+      setMovieData(response.data);
+    } catch (error) {
+      console.error("Error submitting rating:", error);
+    }
+  };
+
   return (
     <>
-    <Nav></Nav>
-    <div className="movie-info">
-      <h1>영화 정보</h1>
-      {movieData && (
-        <div className="All-content">
-          <div className="movie-title">
-            <h3>{movieData.title_kor}</h3>
-            <p>{movieData.title_eng}</p>
-          </div>
-          <div className="movie-content">
-            <img
-              src={movieData.poster_url}
-              alt={movieData.title_kor}
-              onClick={() => handleMovieClick(movieData)}
-              style={{ cursor: "pointer" }}
-            />
-            <div className="movie-text">
-              {selectedMovie && (
-                <div className="movie-details">
-                  <p>[평점] {movieData.rating}</p>
-                  <p>[감독] {movieData.director_name}</p>
-                  <p>[장르] {movieData.genre}</p>
-                  <p>[상영시간] {movieData.showtime}분</p>
-                  <p>[개봉일] {movieData.release_date}</p>
-                  <p>[줄거리]</p>
-                  <p>{movieData.plot}</p>
-                </div>
-              )}
+      <Nav></Nav>
+      <div className="movie-info">
+        <h1>영화 정보</h1>
+        {movieData && (
+          <div className="All-content">
+            <div className="movie-title">
+              <h3>{movieData.title_kor}</h3>
+              <p>{movieData.title_eng}</p>
             </div>
-          </div>
-          <h4>[인물정보]</h4>
-          <div className="actor-list">
-            {movieData.actors.map((actor, index) => (
-              <div key={index} className="actor-item">
-                <img
-                  src={actor.image_url}
-                  alt={actor.name}
-                  style={{ maxWidth: "100px" }}
-                />
-                <p>{actor.name}</p>
-                <p>{actor.character}</p>
+            <div className="movie-content">
+              <img
+                src={movieData.poster_url}
+                alt={movieData.title_kor}
+                onClick={() => handleMovieClick(movieData)}
+                style={{ cursor: "pointer" }}
+              />
+              <div className="movie-text">
+                {selectedMovie && (
+                  <div className="movie-details">
+                    <p>[평점] {movieData.rating}</p>
+                    <p>[감독] {movieData.director_name}</p>
+                    <p>[장르] {movieData.genre}</p>
+                    <p>[상영시간] {movieData.showtime}분</p>
+                    <p>[개봉일] {movieData.release_date}</p>
+                    <p>[줄거리]</p>
+                    <p>{movieData.plot}</p>
+                  </div>
+                )}
               </div>
-            ))}
-          </div>
-          <h4>댓글을 남겨보세요 !</h4>
-          <div id="comment-box">
-            <div className="text-box">
-              <textarea
-                id="comment-input"
-                placeholder="댓글을 입력하세요."
-                value={comment}
-                onChange={handleCommentChange}
-              ></textarea>
-              <button id="comment-submit" onClick={handleCommentSubmit}>
-                댓글 작성
-              </button>
             </div>
-            <div id="comment-list">
-              {comments.map((comment, index) => (
-                <div key={index} className="comment-item">
-                  <p>
-                    NICKNAME : {comment.nickname}{" "}
-                    : {comment.comment}
-                  </p>
+            <h4>[인물정보]</h4>
+            <div className="actor-list">
+              {movieData.actors.map((actor, index) => (
+                <div key={index} className="actor-item">
+                  <img
+                    src={actor.image_url}
+                    alt={actor.name}
+                    style={{ maxWidth: "100px" }}
+                  />
+                  <p>{actor.name}</p>
+                  <p>{actor.character}</p>
                 </div>
               ))}
             </div>
+            <div className="Rating-box">
+              <h4>별점을 남겨보세요!</h4>
+              <StarRating onRatingSubmit={handleRatingSubmit} />
+            </div>
+            <h4>댓글을 남겨보세요!</h4>
+            <div id="comment-box">
+              <div className="text-box">
+                <textarea
+                  id="comment-input"
+                  placeholder="댓글을 입력하세요."
+                  value={comment}
+                  onChange={handleCommentChange}
+                ></textarea>
+                <button id="comment-submit" onClick={handleCommentSubmit}>
+                  댓글 작성
+                </button>
+              </div>
+              <div id="comment-list">
+                {comments.map((comment, index) => (
+                  <div key={index} className="comment-item">
+                    <p>
+                      NICKNAME : {comment.nickname} : {comment.comment}
+                    </p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
     </>
   );
 }
---
+++ src/StarRating.js
@@ -0,0 +1,28 @@
+
+import React, { useState } from "react";
+import "./StarRating.css";
+
+function StarRating({ onRatingSubmit }) {
+  const [rating, setRating] = useState(0);
+
+  const handleStarClick = (starIndex) => {
+    setRating(starIndex);
+    onRatingSubmit(starIndex); 
+  };
+
+  return (
+    <div className="star-rating">
+      {[1, 2, 3, 4, 5].map((star) => (
+        <span
+          key={star}
+          className={`star ${star <= rating ? "selected" : ""}`}
+          onClick={() => handleStarClick(star)}
+        >
+            ★  
+        </span>
+      ))}
+    </div>
+  );
+}
+
+export default StarRating;
