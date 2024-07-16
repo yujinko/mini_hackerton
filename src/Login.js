676efeaaa231bb67b1e2397bed1c245d86066584
yujinko <rhdbwls3355@naver.com>
2024-07-16 09:20:45 +0900

    회원가입, 로그인 성공

--- src/Login.js
+++ src/Login.js
@@ -1,49 +1,52 @@
 import React, { useState } from "react";
-import { 
-  SignUpContainer, 
-  TitleContainer, 
-  InputStyle, 
+import {
+  SignUpContainer,
+  TitleContainer,
+  InputStyle,
   ButtonContainer,
-  LinkLogin
+  LinkLogin,
 } from "./Styled";
-import { Link, useNavigate } from 'react-router-dom'; 
+import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
 export default function Login() {
-  const [id, setId] = useState('');
-  const [pw, setPw] = useState('');
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
   const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
 
   const login = async () => {
     try {
-        const response = await axios.post('https://hufs-mutsa-12th.store/dj/login/', {
-            username: id,
-            password: pw,
-        });
-        if (response.data && response.data.access) {
-            console.log(response.data);
-            localStorage.setItem("access", response.data.access);
-            setShowModal(true); // 로그인 성공 시 모달을 표시
-            navigate('/Main'); // 로그인 성공 시 메인 페이지로 이동
-        } else {
-            alert('로그인 실패');
+      const response = await axios.post(
+        "https://minihackton.store/member/login/",
+        {
+          username: id,
+          password: pw,
         }
+      );
+      if (response.data && response.data.access) {
+        console.log(response.data);
+        localStorage.setItem("access", response.data.access);
+        setShowModal(true); // 로그인 성공 시 모달을 표시
+        navigate("/"); // 로그인 성공 시 메인 페이지로 이동
+      } else {
+        alert("로그인 실패");
+      }
     } catch (error) {
-        console.log(error.response);
-        alert('로그인 실패');
+      console.log(error.response);
+      alert("로그인 실패");
     }
   };
 
   function IdChange(e) {
     console.log(e);
     setId(e.target.value);
-  };
+  }
 
   function PwChange(e) {
     console.log(e);
     setPw(e.target.value);
-  };
+  }
 
   const closeModal = () => {
     setShowModal(false);
@@ -53,18 +56,18 @@ export default function Login() {
     <SignUpContainer>
       <TitleContainer>로그인</TitleContainer>
       <div>
-        <InputStyle 
+        <InputStyle
           placeholder="아이디를 입력해주세요"
           value={id}
           onChange={IdChange}
         />
-        <InputStyle 
+        <InputStyle
           placeholder="비밀번호를 입력해주세요"
           value={pw}
           onChange={PwChange}
         />
       </div>
-      
+
       <ButtonContainer onClick={login}>로그인하기</ButtonContainer>
       <LinkLogin>
         <Link to="/signup">회원가입</Link>
@@ -79,5 +82,5 @@ export default function Login() {
         </div>
       )}
     </SignUpContainer>
-  )
+  );
 }
